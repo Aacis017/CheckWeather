58a1cf1941fe11359ed24c2e2e4fd515e1517148
Aacis017 <dhakalaashish17@gmail.com>
2024-08-04 14:26:14 +0545

    api connected

--- CheckWeather/Models/Root.cs
+++ CheckWeather/Models/Root.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,65 +7,81 @@ using System.Threading.Tasks;
 
 namespace CheckWeather.Models
 {
-    public class Clouds
+    // Root myDeserializedClass = JsonConvert.DeserializeObject<Root>(myJsonResponse);
+    public class AirQuality
     {
-        public int all { get; set; }
+        public double co { get; set; }
+        public int no2 { get; set; }
+        public double o3 { get; set; }
+        public double so2 { get; set; }
+        public double pm2_5 { get; set; }
+        public double pm10 { get; set; }
+
+        [JsonProperty("us-epa-index")]
+        public int usepaindex { get; set; }
+
+        [JsonProperty("gb-defra-index")]
+        public int gbdefraindex { get; set; }
     }
 
-    public class Coord
+    public class Condition
     {
-        public double lon { get; set; }
+        public string text { get; set; }
+        public string icon { get; set; }
+        public int code { get; set; }
+    }
+
+    public class Current
+    {
+        public int last_updated_epoch { get; set; }
+        public string last_updated { get; set; }
+        public double temp_c { get; set; }
+        public double temp_f { get; set; }
+        public int is_day { get; set; }
+        public Condition condition { get; set; }
+        public double wind_mph { get; set; }
+        public double wind_kph { get; set; }
+        public double wind_degree { get; set; }
+        public string wind_dir { get; set; }
+        public double pressure_mb { get; set; }
+        public double pressure_in { get; set; }
+        public double precip_mm { get; set; }
+        public double precip_in { get; set; }
+        public double humidity { get; set; }
+        public double cloud { get; set; }
+        public double feelslike_c { get; set; }
+        public double feelslike_f { get; set; }
+        public double windchill_c { get; set; }
+        public double windchill_f { get; set; }
+        public double heatindex_c { get; set; }
+        public double heatindex_f { get; set; }
+        public double dewpoint_c { get; set; }
+        public double dewpoint_f { get; set; }
+        public double vis_km { get; set; }
+        public double vis_miles { get; set; }
+        public double uv { get; set; }
+        public double gust_mph { get; set; }
+        public double gust_kph { get; set; }
+        public AirQuality air_quality { get; set; }
+    }
+
+    public class Location
+    {
+        public string name { get; set; }
+        public string region { get; set; }
+        public string country { get; set; }
         public double lat { get; set; }
-    }
-
-    public class Main
-    {
-        public double temp { get; set; }
-        public double feels_like { get; set; }
-        public double temp_min { get; set; }
-        public double temp_max { get; set; }
-        public int pressure { get; set; }
-        public int humidity { get; set; }
-        public int sea_level { get; set; }
-        public int grnd_level { get; set; }
+        public double lon { get; set; }
+        public string tz_id { get; set; }
+        public int localtime_epoch { get; set; }
+        public string localtime { get; set; }
     }
 
     public class Root
     {
-        public Coord coord { get; set; }
-        public List<Weather> weather { get; set; }
-        public string @base { get; set; }
-        public Main main { get; set; }
-        public int visibility { get; set; }
-        public Wind wind { get; set; }
-        public Clouds clouds { get; set; }
-        public int dt { get; set; }
-        public Sys sys { get; set; }
-        public int timezone { get; set; }
-        public int id { get; set; }
-        public string name { get; set; }
-        public int cod { get; set; }
+        public Location location { get; set; }
+        public Current current { get; set; }
     }
 
-    public class Sys
-    {
-        public string country { get; set; }
-        public int sunrise { get; set; }
-        public int sunset { get; set; }
-    }
 
-    public class Weather
-    {
-        public int id { get; set; }
-        public string main { get; set; }
-        public string description { get; set; }
-        public string icon { get; set; }
-    }
-
-    public class Wind
-    {
-        public double speed { get; set; }
-        public int deg { get; set; }
-        public double gust { get; set; }
-    }
 }
--- CheckWeather/WeatherAlert.xaml.cs
+++ CheckWeather/WeatherAlert.xaml.cs
@@ -1,3 +1,4 @@
+using CheckWeather.Services;
 using Microsoft.Maui.Controls;
 using System;
 
@@ -5,36 +6,26 @@ namespace CheckWeather
 {
     public partial class WeatherAlert : ContentPage
     {
-        private Random _random;
-        private readonly TimeSpan _updateInterval = TimeSpan.FromSeconds(5); // Update every 5 seconds
-
         public WeatherAlert()
         {
             InitializeComponent();
-            _random = new Random();
-            StartUpdatingWeatherData();
         }
 
-        private async void StartUpdatingWeatherData()
+        protected async override void OnAppearing()
         {
-            while (true)
-            {
-                GenerateRandomWeatherData();
-                await Task.Delay(_updateInterval);
-            }
-        }
 
-        private void GenerateRandomWeatherData()
-        {
-            double windSpeed = _random.NextDouble() * 20; 
-            double precipitation = _random.NextDouble() * 20; 
 
-            LblWind.Text = $"{windSpeed:F1} m/s";
-            LblRain.Text = $"{precipitation:F1} mm/hr";
+            base.OnAppearing();
+            var result = await ApiService.GetCurrentWeather(27.7899, 83.501);
+            LblRain.Text = result.current.precip_mm.ToString() + "mm/hr";
 
-            bool isWindSpeedHigh = windSpeed > 8;
-            bool isPrecipitationHigh = precipitation > 10;
+            double windSpeedMps = result.current.wind_mph * 0.44704;
 
+            LblWind.Text = windSpeedMps.ToString("F2")+" m/s";
+
+
+            bool isPrecipitationHigh = result.current.precip_mm > 10;
+            bool isWindSpeedHigh = windSpeedMps > 8;
             AlertLabel.IsVisible = isWindSpeedHigh || isPrecipitationHigh;
 
             if (isWindSpeedHigh && isPrecipitationHigh)
@@ -51,4 +42,66 @@ namespace CheckWeather
             }
         }
     }
-}
+
+
+        //    private Random _random;
+        //    private readonly TimeSpan _updateInterval = TimeSpan.FromSeconds(5); // Update every 5 seconds
+
+        //    public WeatherAlert()
+        //    {
+        //        InitializeComponent();
+        //        _random = new Random();
+        //        StartUpdatingWeatherData();
+        //    }
+
+        //    private async void StartUpdatingWeatherData()
+        //    {
+        //        while (true)
+        //        {
+        //            GenerateRandomWeatherData();
+        //            await Task.Delay(_updateInterval);
+        //        }
+        //    }
+
+        //    private void GenerateRandomWeatherData()
+        //    {
+        //        double windSpeed = _random.NextDouble() * 20; 
+        //        double precipitation = _random.NextDouble() * 20; 
+
+        //        LblWind.Text = $"{windSpeed:F1} m/s";
+        //        LblRain.Text = $"{precipitation:F1} mm/hr";
+
+        //        bool isWindSpeedHigh = windSpeed > 8;
+        //        bool isPrecipitationHigh = precipitation > 10;
+
+        //        AlertLabel.IsVisible = isWindSpeedHigh || isPrecipitationHigh;
+
+        //        if (isWindSpeedHigh && isPrecipitationHigh)
+        //        {
+        //            AlertLabel.Text = "Warning: High wind speed and heavy precipitation!";
+        //        }
+        //        else if (isWindSpeedHigh)
+        //        {
+        //            AlertLabel.Text = "Warning: Wind speed is higher than 8 m/s!";
+        //        }
+        //        else if (isPrecipitationHigh)
+        //        {
+        //            AlertLabel.Text = "Warning: Precipitation is more than 10 mm/hr!";
+        //        }
+        //    }
+
+
+        //    private void OnFetchDataClicked(object sender, EventArgs e)
+        //    {
+        //        // Retrieve latitude and longitude values from Entry fields
+        //        string latitude = EntryLatitude.Text;
+        //        string longitude = EntryLongitude.Text;
+
+        //        // Perform your data fetching logic here
+        //        // You can use latitude and longitude values to fetch weather data
+
+        //        // For demonstration, showing an alert with the entered values
+        //        DisplayAlert("Fetching Data", $"Latitude: {latitude}, Longitude: {longitude}", "OK");
+        //    }
+        //}
+    }
